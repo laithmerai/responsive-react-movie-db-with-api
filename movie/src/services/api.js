aa94dc4695aec3ec271e1d783e8d4f2de09393df
laithmerai <laithmerai@gmail.com>
2025-06-03 16:34:57 +0300

    all

---
+++ movie/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "bf0f2d3e56684544302f5be8d167dd7b";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
